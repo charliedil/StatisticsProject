9a5681bc45bed6013c559a05f047bcc7de5371c4
charliedil <charliedil>
2021-11-22 19:18:13 -0500

    Added sampling.py which has the code for doing SRS and block SRS. Also added params for sample_size (SRS), num_subgroups (block SRS), and subgroup_size (block SRS). Still working on error checking for these new params.

--- main.py
+++ main.py
@@ -20,25 +20,53 @@ if __name__ == '__main__':
     parser.add_argument('--csv', type=str, help="Provide path to input csv. Required if no path to pdf is provided")
     parser.add_argument('--output', type=str, help="Provide path to where processed data should go. Default is data "
                                                    "subdirectory")
+    parser.add_argument("--block", type=str, help="If you want to do block SRS, what category do you wish for the "
+                                                  "blocks to be (Options: Sex, teacher, or status). Default will be"
+                                                  "regular SRS.")
+    parser.add_argument("--sample_size", type=int, help="Required for regular SRS. Size of the desired samples.", default=-1)
+    parser.add_argument("--num_subgroups", type=int, help="Required for block SRS. Number of blocks desired.", default=-1)
+    parser.add_argument("--subgroup_size", type=int, help="Required for block SRS. Size of subgroup samples.",default=-1)
     args = parser.parse_args()
 
     pdf_path = args.pdf
     csv_path = args.csv
     output_path = args.output
+    block = args.block
+    sample_size = args.sample_size
+    num_subgroups = args.num_subgroups
+    subgroup_size = args.subgroup_size
+    categories = ["sex", "teacher", "status"]
+
+    # check for whether we will be doing regular srs or block srs.
+    srs = False
+    if block is None:
+        srs = True
+    else:
+        block = block.lower()
     scraper = True
-    print(pdf_path)
+
     # Flags for whether paths are given
     if pdf_path is None and csv_path is not None:
         scraper = False
     if output_path is None:
         output_path = "data/"
+
     # Error handling
     if pdf_path is None and csv_path is None:
         print("Either pdf path or csv path must be provided.")
         exit(-1)
+    if not srs and block not in categories:
+        print("If block srs, then the category should be either sex, teacher, or status")
+        exit(-1)
+    if not srs and
+
     # actual algorithm
     if scraper:
         tabula.convert_into(pdf_path, output_path + "studentData.csv", pages='all')
         csv_path = output_path + "studentData.csv"
     students = parse_csv(csv_path)
-    print(students)
+
+    if srs:
+        students
+
+
---
+++ sampling.py
@@ -0,0 +1,83 @@
+from random import randrange
+
+def srs(entities, sample_size):
+    if len(entities) < sample_size: # check sample size
+        print("ERROR: sample size cannot be bigger than number of entities")
+        exit(-1)
+    entity_sample = []
+    chosen_numbers = []
+    for i in range(sample_size):
+        index = randrange(sample_size)
+        while index in chosen_numbers:
+            index = randrange(sample_size)
+        entity_sample.append(entities[index])
+        chosen_numbers.append(index)
+    return entity_sample
+
+def block_srs(students, num_subgroups, subgroup_size, category):
+    entity_sample = []
+    if category == "teacher":
+        stud_to_teacher = {}
+        teacher_min = {}
+        for student in students:
+            if student.teacher not in stud_to_teacher:
+                stud_to_teacher[student.teacher] = []
+                teacher_min[student.teacher] = 0
+            stud_to_teacher[student.teacher].append(student)
+            teacher_min[student.teacher] += 1
+        if len(stud_to_teacher) < num_subgroups:
+            print("ERROR: number of subgroups cannot exceed the block category")
+            exit(-1)
+        if min(list(teacher_min.values())) < subgroup_size:
+            print("ERROR: subgroup size cannot exceed minimum amount of students assigned to any individual block "
+                  "category")
+            exit(-1)
+        teachers = srs(list(stud_to_teacher.keys()), num_subgroups)
+        for teacher in teachers:
+            students_of_teacher_sample = srs(stud_to_teacher[teacher], subgroup_size)
+            for s in students_of_teacher_sample:
+                entity_sample.append(s)
+    elif category == "sex":
+        stud_to_sex = {}
+        sex_min = {}
+        for student in students:
+            if student.sex not in stud_to_sex:
+                stud_to_sex[student.sex] = []
+                sex_min[student.sex] = 0
+            stud_to_sex[student.sex].append(student)
+            sex_min[student.sex] += 1
+        if len(stud_to_sex) < num_subgroups:
+            print("ERROR: number of subgroups cannot exceed the block category")
+            exit(-1)
+        if min(list(sex_min.values())) < subgroup_size:
+            print("ERROR: subgroup size cannot exceed minimum amount of students assigned to any individual block "
+                  "category")
+            exit(-1)
+        sexes = srs(list(stud_to_sex.keys()), num_subgroups)
+        for sex in sexes:
+            students_of_sex_sample = srs(stud_to_sex[sex], subgroup_size)
+            for s in students_of_sex_sample:
+                entity_sample.append(s)
+    else:
+        stud_to_status = {}
+        status_min = {}
+        for student in students:
+            if student.status not in stud_to_status:
+                stud_to_status[student.status] = []
+                status_min[student.status] = 0
+            stud_to_status[student.status].append(student)
+            status_min[student.status] += 1
+        if len(stud_to_status) < num_subgroups:
+            print("ERROR: number of subgroups cannot exceed the block category")
+            exit(-1)
+        if min(list(status_min.values())) < subgroup_size:
+            print("ERROR: subgroup size cannot exceed minimum amount of students assigned to any individual block "
+                  "category")
+            exit(-1)
+        statuses = srs(list(stud_to_status.keys()), num_subgroups)
+        for status in statuses:
+            students_of_status_sample = srs(stud_to_status[status], subgroup_size)
+            for s in students_of_status_sample:
+                entity_sample.append(s)
+
+
